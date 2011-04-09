f890c41f376873083d17e0d0064829342dfb7fe9
railspundit <railspundit@railspundit-laptop.(none)>
2011-04-09 13:17:21 +0530

    add restaurants

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,18 @@
+class RestaurantsController < ApplicationController
+def index
+@restaurants = Restaurant.all
+end
+def new
+end
+def show
+end
+def create
+end
+def edit
+end
+def update
+end
+def destroy
+end
+end
+
