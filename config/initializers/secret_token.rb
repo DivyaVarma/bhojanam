0d3434c0f334db494a5e2354d90162f476184e87
railspundit <railspundit@railspundit-laptop.(none)>
2011-04-09 12:52:32 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BhojanamPlain::Application.config.secret_token = '199510d9b50671ec59444d3f4b6d96da7e5d8b0d37fa93825dce832cc7617d3fe134f9fd9ec8d425e9c543340a4dd7b1343ce4938a347a2fab2e7151af5a3cb3'
